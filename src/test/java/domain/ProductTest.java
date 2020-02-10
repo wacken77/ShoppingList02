ff9744ad17f80bb83199d851351ee1675ad635b9
javagurulv <javagurulv@gmail.com>
2020-02-10 10:26:44 +0200

    Update ProductTest.java

--- src/test/java/domain/ProductTest.java
+++ src/test/java/domain/ProductTest.java
@@ -6,8 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    // Write tests for each class like:  ProductNameValidationRule, ProductValidationService, ProductInMemoryRepository
+    
+    
+    // What this test check??  Where assertions? Please write proper test case:
+    // 1. prepare test data
+    // 2. invoke production code
+    // 3. assert results
+    
     @org.junit.jupiter.api.Test
-    void getReducedPrice() throws Exception {
+    void getReducedPrice() throws Exception {   
         Product testProduct = new Product();
         testProduct.setPrice(BigDecimal.valueOf(100));
         testProduct.setDiscount(15);
